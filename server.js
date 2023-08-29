463292bbb677833d326ef0d5089fa033237209ea
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2023-08-29 16:04:50 +0530

    Typo error

--- server.js
+++ server.js
@@ -38,8 +38,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
   );
-  r;
-  es.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
