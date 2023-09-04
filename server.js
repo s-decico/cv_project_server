7248bc3191a794c6ac8e9c16d0e1d439c4ef3f04
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2023-09-04 16:29:06 +0530

    commenting expose header

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept,Authorization"
   );
-  res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
+  // res.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
 
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
