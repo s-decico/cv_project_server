f310134cd5fd784c1b66f7249eea03b85ea94654
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2023-09-11 22:33:34 +0530

    setting token in fetchform

--- server.js
+++ server.js
@@ -215,8 +215,9 @@ app.route("/login").post((req, res) => {
 
 app.route("/fetchform").get((req, res) => {
   console.log("Fetching details of form triggered...");
-  const token = req.cookies.token;
-  console.log("request:", req);
+  // const token = req.cookies.token;
+  const token = req.body.token;
+  // console.log("request:", req);
   console.log("Token", token);
   if (token) {
     decodedToken = verifyToken(token, process.env.JWT_SECRET_KEY);
