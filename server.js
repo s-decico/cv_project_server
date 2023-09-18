521e16199eedc3dbb9107519c54acafb20385af5
Syamantak Sarkar <49286743+s-decico@users.noreply.github.com>
2023-09-18 20:23:49 +0530

    Token from auth header

--- server.js
+++ server.js
@@ -215,8 +215,8 @@ app.route("/login").post((req, res) => {
 
 app.route("/fetchform").get((req, res) => {
   console.log("Fetching details of form triggered...", req.headers);
-  const token = req.cookies.token;
-  // const token = req.headers.authorization?.split(" ")[1];
+  // const token = req.cookies.token;
+  const token = req.headers.authorization?.split(" ")[1];
   if (token) {
     decodedToken = verifyToken(token, process.env.JWT_SECRET_KEY);
     console.log("Decoded Token:", decodedToken);
